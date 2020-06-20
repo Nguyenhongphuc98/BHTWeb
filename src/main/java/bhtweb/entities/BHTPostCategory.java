a83970b815711ad49d9148e2a46a9bb41575968e
Nghi <luubieunghi@gmail.com>
2020-06-20 10:39:53 +0700

    Add entities

---
+++ src/main/java/bhtweb/entities/BHTPostCategory.java
@@ -0,0 +1,20 @@
+package bhtweb.entities;
+
+public class BHTPostCategory {
+
+	private Long postCategoryID;
+	private String postCategoryName;
+	
+	public Long getPostCategoryID() {
+		return postCategoryID;
+	}
+	public void setPostCategoryID(Long postCategoryID) {
+		this.postCategoryID = postCategoryID;
+	}
+	public String getPostCategoryName() {
+		return postCategoryName;
+	}
+	public void setPostCategoryName(String postCategoryName) {
+		this.postCategoryName = postCategoryName;
+	}
+}
