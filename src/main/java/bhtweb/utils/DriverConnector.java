67f528600e44a570065b25f88499bfa770221fe0
nguyenhongphuc98 <nguyenhongphuc98@gmail.com>
2020-07-21 22:58:34 +0700

    fix upload 1

--- src/main/java/bhtweb/utils/DriverConnector.java
+++ src/main/java/bhtweb/utils/DriverConnector.java
@@ -57,7 +57,6 @@ public class DriverConnector {
         GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
                 HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
                 .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
                 .build();
         LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
         return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
